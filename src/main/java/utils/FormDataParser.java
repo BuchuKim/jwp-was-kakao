694118152a36dad99793d283f8b9f39f7a92d986
buchukim <qkqdlek32@gmail.com>
2024-04-11 16:13:54 +0900

    refactor: 쿼리 파라미터 예외처리 추가

--- src/main/java/utils/FormDataParser.java
+++ src/main/java/utils/FormDataParser.java
@@ -7,6 +7,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FormDataParser {
+    private static final String AMPERSAND = "&";
+    private static final String EQUAL = "=";
+
     public static Map<String, String> parse(final String dataString) {
         Map<String, String> queryParameters = new HashMap<>();
         
@@ -14,12 +17,20 @@ public class FormDataParser {
             return queryParameters;
         }
         
-        final String[] parameters = dataString.split("&");
+        final String[] parameters = dataString.split(AMPERSAND);
         Arrays.stream(parameters).forEach(parameter -> {
-            final String[] keyValue = parameter.split("=");
-            queryParameters.put(URLDecoder.decode(keyValue[0], StandardCharsets.UTF_8),
-                URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8));
+            Map.Entry<String, String> queryEntry = getQueryEntry(parameter);
+            queryParameters.put(URLDecoder.decode(queryEntry.getKey(), StandardCharsets.UTF_8),
+                URLDecoder.decode(queryEntry.getValue(), StandardCharsets.UTF_8));
         });
         return queryParameters;
+    }
+
+    private static Map.Entry<String, String> getQueryEntry(String parameter) {
+        int splitIndex = parameter.indexOf(EQUAL);
+        if (splitIndex > 0) {
+            return Map.entry(parameter.substring(0, splitIndex), parameter.substring(splitIndex + 1));
+        }
+        return Map.entry(parameter, "");
     }
 }
--- src/test/java/utils/FormDataParserTest.java
+++ src/test/java/utils/FormDataParserTest.java
@@ -1,6 +1,7 @@
 package utils;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -34,7 +35,42 @@ class FormDataParserTest {
     @ParameterizedTest
     @NullAndEmptySource
     void 쿼리스트링이_없을때_쿼리파라미터_정보가_없다(final String input) {
-        System.out.println("input: " + input);
         assertThat(FormDataParser.parse(input).keySet().isEmpty()).isTrue();
+    }
+
+    @Test
+    void 첫번째_equal싸인까지가_쿼리파라미터의_key이다() {
+        // given
+        final String userId = "a=b";
+        final String name = "이동규";
+        final String formDataString = "userId=" + userId + "&name=" + name;
+        System.out.println(formDataString);
+
+        // when
+        final Map<String, String> parsedData = FormDataParser.parse(formDataString);
+
+        assertAll(
+            () -> assertThat(parsedData.get("userId")).isEqualTo(userId),
+            () -> assertThat(parsedData.get("name")).isEqualTo(name)
+        );
+    }
+
+    @Test
+    void equal싸인이_없으면_키만_추가한다() {
+        // given
+        final String key1 = "key1";
+        final String key2 = "key2";
+        final String formDataString = key1 + "&" + key2;
+        System.out.println(formDataString);
+
+        // when
+        final Map<String, String> parsedData = FormDataParser.parse(formDataString);
+        parsedData.forEach((k,v) -> System.out.println(k + " : " + v));
+
+        // then: 키만 추가한 값의 value는 empty string이다.
+        assertAll(
+            () -> assertThat(parsedData.get(key1)).isEmpty(),
+            () -> assertThat(parsedData.get(key2)).isEmpty()
+        );
     }
 }
