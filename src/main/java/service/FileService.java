80d71f650a924585770ea581aeddf11af9827855
buchukim <qkqdlek32@gmail.com>
2024-04-16 10:30:59 +0900

    feat: `ResponseEntity#addCookie` 추가

--- src/main/java/service/FileService.java
+++ src/main/java/service/FileService.java
@@ -11,6 +11,6 @@ public class FileService {
     public ResponseEntity serveFile(final RequestHeader request) throws IOException, URISyntaxException {
         String path = request.getPath();
         byte[] staticFileData = FileIoUtils.loadFileFromClasspath(path);
-        return ResponseEntity.of(path, staticFileData);
+        return ResponseEntity.fileDataOf(path, staticFileData);
     }
 }
--- src/main/java/webserver/entity/response/ResponseEntity.java
+++ src/main/java/webserver/entity/response/ResponseEntity.java
@@ -1,29 +1,42 @@
 package webserver.entity.response;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import webserver.FileExtension;
+import webserver.entity.HttpCookie;
 import webserver.entity.StatusCode;
 
 public class ResponseEntity {
     private final StatusCode statusCode;
     private final Map<String, String> headers;
+    private final List<HttpCookie> cookies;
     private final byte[] body;
-    
+
     private ResponseEntity(final StatusCode statusCode, final Map<String, String> headers, final byte[] body) {
         this.statusCode = statusCode;
         this.headers = headers;
+        this.cookies = new ArrayList<>();
         this.body = body;
     }
     
     private ResponseEntity(final StatusCode statusCode, final Map<String, String> headers) {
         this.statusCode = statusCode;
         this.headers = headers;
+        this.cookies = new ArrayList<>();
         this.body = new byte[0];
     }
+
+    public static ResponseEntity htmlDataOf(final String body) {
+        return new ResponseEntity(StatusCode.OK,
+            Map.of("Content-Type", "text/html;charset=utf-8"),
+            body.getBytes(StandardCharsets.UTF_8));
+    }
     
-    public static ResponseEntity of(final String path, final byte[] body) {
+    public static ResponseEntity fileDataOf(final String path, final byte[] body) {
         return new ResponseEntity(StatusCode.OK,
             Map.of("Content-Type", FileExtension.convertToContentType(path) + ";charset=utf-8",
                 "Content-Length", String.valueOf(body.length)),
@@ -44,12 +57,17 @@ public class ResponseEntity {
         return new ResponseEntity(StatusCode.INTERNAL_SERVER_ERROR,
             Map.of());
     }
+
+    public void addCookie(final HttpCookie cookie) {
+        cookies.add(cookie);
+    }
     
     public String toResponseMessage() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("HTTP/1.1 ");
         stringBuilder.append(statusCode.generateResponseLine());
         headers.forEach((key, value) -> stringBuilder.append(key).append(": ").append(value).append("\r\n"));
+        cookies.forEach(cookie -> stringBuilder.append(cookie.toSetCookieHeaderString()).append("\r\n"));
         stringBuilder.append("\r\n");
         stringBuilder.append(new String(body, StandardCharsets.ISO_8859_1));
         return stringBuilder.toString();
